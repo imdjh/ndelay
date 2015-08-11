9f231aa9154f8775c88f866ae47c7151cc859364
Dye Jarhoo <20123225@student.shiep.edu.cn>
2015-08-11 15:18:42 +0800

    Report on SIGINT, report lost connect times & rate
    Add dependence of stringformat
    Update TODO list

--- client/sender.js
+++ client/sender.js
@@ -1,9 +1,14 @@
 var net = require('net');
+var stringformat = require('stringformat');
+
+// Enable format stings syntax
+stringformat.extendString();
 
 var host = '115.29.160.249';
 var port = 2223;
 var killo = new Buffer(1000, 'binary');
 var timeList = [];
+var errCount = 0;
 
 var stamp1, stamp2;
 
@@ -12,6 +17,13 @@ var client = new net.Socket();
 // satify the watchdog
 client.setMaxListeners(0);
 
+// connection error handling
+client.on('error', function (e) {
+	// DEBUG: console.log('Connection error happens');
+	errCount++;
+	client.destroy();
+});
+	
 client.on('data', function (d) {
         // TODO: check the data first
         //
@@ -23,7 +35,7 @@ client.on('data', function (d) {
 
         // DEBUG: console.log('diff tmie: ' + (stamp2 - stamp1) );
 	// Calculate the average
-	avg();
+	// DEBUG: VERBOSE: avg();
 
 	// Mission complete
         client.destroy();
@@ -31,7 +43,7 @@ client.on('data', function (d) {
 });
 
 client.on('close', function (c) {
-        console.log('Self destruct Successful!');
+        // DEBUG: console.log('Self destruct Successful!');
 });
 
 // TODO: loop for MAGIC times
@@ -52,5 +64,22 @@ var sum = 0;
 for (var i = 0; i < timeList.length; i++) {
 	sum += timeList[i];
 }
-console.log('Average is :' + (sum / timeList.length));
+	console.log('Average is :' + (sum / timeList.length));
 }
+
+// Give result on SIGINT
+process.on('SIGINT', function () {
+	avg();
+	
+	// Output ERR connection times & rate
+	if (errCount) {
+		console.log('ERR happen: ' + errCount + ' times!');
+		var stringErrRate = 'ERR rate: {0}/{1} = {2}'.format(errCount, timeList.length, (errCount/timeList.length));
+		console.log(stringErrRate);
+	}
+
+
+	// Output worst delay
+	console.log('Max delay: ' + Math.max.apply(null, timeList));
+	process.exit();
+});
