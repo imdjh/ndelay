b9f2aaba9dc937caefd62971f5b1757be7fc64b0
Dye Jarhoo <20123225@student.shiep.edu.cn>
2015-08-13 23:10:17 +0800

    rewrite server in es6 spec, add cli support

--- server/listen.js
+++ server/listen.js
@@ -1,28 +1,72 @@
-var net = require('net');
-//var util = require('util');
+#!/usr/bin/env node
+"use strict"
 
-var host = '0.0.0.0';
-var port = 2223;
-var killo = new Buffer(1000, 'binary');
+// META-data area
+// author: dyejarhoo@gmail.com
+// lang-spec: es6
+//
+
+/**
+ *  Module dependencies
+ */
+var net = require('net');
+var program = require('commander');
+
+
+/**
+ * Parse argv
+ */
+program
+  .version('0.1.1')
+  .usage('[-v]')
+  .option('-v, --verbose', 'Verbose output to console')
+  .option('-l, --listen <ip>', 'Listen connection to <ip>, default to all', '0.0.0.0')
+  .option('-p, --port <port>', 'ndelay stays at <port>, default to 8100', 8100)
+  .option('--package-size <kb>', 'Sends <kb> size package on receiving client connection, default to 1000', 1000)
+  .parse(process.argv);
+
+if (program.args.length) {
+	program.outputHelp();
+	process.exit(1);
+}
+
+/**
+ * Initialize values
+ */
+var buf = new Buffer(program.packageSize, 'binary');
+var isverbose = 0;
+if (program.verbose) {
+	isverbose = 1;
+	var util = require('util');
+}
 
 net.createServer(function (sock) {
-        console.log('Connection established from: ' + sock.remoteAddress + ':' + sock.remotePort);
+	let rAddr = sock.remoteAddress;
+	let rPort = sock.remotePort;
 
-        // on receive data
-        sock.on('data', function (d) {
-                // TODO: check the data length is 1kb
-                // var bl = Buffer.byteLength(d, 'binary');
-                //
-                // DEBUG: console.log('yes, the type is: ' + util.inspect(d));
-                //
-                // write back 1kb
-                sock.write(killo);
-        });
 
-        sock.on('close', function (c) {
-                console.log('Connection closed from: ' + sock.remoteAddress + ':' + sock.remotePort);
-        });
-}).listen(port, host);
+	sock.on('data', function (d) {
+		if (program.verbose) {
+			console.log('Connection established from: ' + rAddr + ':' + rPort);
+		}
 
-console.log('Server started on: ' + host + ':' + port);
+		// TODO: check the data length is 1kb
+		// var bl = Buffer.byteLength(d, 'binary');
+		//
+		if (program.verbose) {
+			console.log('data type is: ' + typeof d);
+			console.log('data content is: ' + util.inspect(d));
+		}
 
+		// all ok, write back
+		sock.write(buf);
+	});
+	
+	sock.on('close', function (c) {
+		if (program.verbose) {
+			console.log('Connection closed from: ' + rAddr + ':' + rPort);
+		}
+	});
+}).listen(program.port, program.listen);
+
+console.log('server running @ ' + program.listen + ':' + program.port);
